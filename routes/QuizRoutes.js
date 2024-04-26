031e3bfc29ec046344ca8a878d3250fa0a885b5c
Filip Agatić <128238667+Filip-A25@users.noreply.github.com>
2024-04-26 22:03:19 +0200

    Remove restrictAccess in QuizRoutes

--- routes/QuizRoutes.js
+++ routes/QuizRoutes.js
@@ -12,7 +12,7 @@ quizRouter.route('/all').get(quizController.getAllQuizzes);
 quizRouter.route('/quiz/:id').get(quizController.getQuiz);
 
 
-quizRouter.use(restrictAccess);
+//quizRouter.use(restrictAccess);
 quizRouter.route('/create-new-quiz').post(quizController.createQuiz);
 quizRouter.route('/my-quizzes').get(quizController.getUserQuizzes);
 quizRouter.route('/update-quiz/:id').patch(quizController.updateQuiz);
