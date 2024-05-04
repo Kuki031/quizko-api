a8a861387f2ab4b8c4a36df467e30af6c97e0bdf
Kuki031 <luka.kukucka1@gmail.com>
2024-05-04 14:35:48 +0200

    Make img resolution 1024x768

--- routes/QuizRoutes.js
+++ routes/QuizRoutes.js
@@ -12,9 +12,9 @@ quizRouter.use(isLoggedIn);
 quizRouter.route('/all').get(quizController.getAllQuizzes);
 quizRouter.route('/quiz/:id').get(quizController.getQuiz);
 
-quizRouter.route('/create-new-quiz').post(prepareMulter, resizeImage(640, 360), quizController.createQuiz);
+quizRouter.route('/create-new-quiz').post(prepareMulter, resizeImage(1024, 768), quizController.createQuiz);
 quizRouter.route('/my-quizzes').get(quizController.getUserQuizzes);
-quizRouter.route('/update-quiz/:id').patch(prepareMulter, resizeImage(640, 360), quizController.updateQuiz);
+quizRouter.route('/update-quiz/:id').patch(prepareMulter, resizeImage(1024, 768), quizController.updateQuiz);
 quizRouter.route('/delete-quiz/:id').delete(quizController.deleteQuiz);
 
 module.exports = quizRouter;
