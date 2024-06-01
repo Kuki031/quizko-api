9464709a6e5eeff414b19f3ddc9cd628cc7b28c4
Kuki031 <luka.kukucka1@gmail.com>
2024-06-01 14:36:30 +0200

    Remove caching from 'my-quizzes'

--- routes/QuizRoutes.js
+++ routes/QuizRoutes.js
@@ -14,9 +14,9 @@ const quizRouter = express.Router();
 
 quizRouter.use(isLoggedIn, hasConfirmedEmail);
 quizRouter.route('/all').get(cache("1 minute"), quizController.getAllQuizzes);
-quizRouter.route('/quiz/:id').get(cache("5 minutes"), quizController.getQuiz);
+quizRouter.route('/quiz/:id').get(quizController.getQuiz);
 quizRouter.route('/create-new-quiz').post(prepareMulter, resizeImage(640, 480, 'quiz'), quizController.createQuiz);
-quizRouter.route('/my-quizzes').get(cache("5 minutes"), quizController.getUserQuizzes);
+quizRouter.route('/my-quizzes').get(cache("2 minutes"), quizController.getUserQuizzes);
 
 quizRouter.route('/update-quiz/:id').patch(hasCreatedQuiz("id"), prepareMulter, resizeImage(640, 480, 'quiz'), quizController.updateQuiz);
 quizRouter.route('/delete-quiz/:id').delete(hasCreatedQuiz("id"), quizController.deleteQuiz);
